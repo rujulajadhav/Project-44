bc63173bb11f84f25c3bbbdf07a18b61708da770
rujulajadhav <82890653+rujulajadhav@users.noreply.github.com>
2021-05-29 17:55:23 +0530

    Add files via upload

---
+++ Security.js
@@ -0,0 +1,21 @@
+class Security {
+    constructor(){
+        this.access1 = createInput("Code1")
+        this.access1.position(100, 90);
+        this.access1.style('background', 'white');
+
+        this.button1 = createButton('Check')
+        this.button1.position(100, 120);
+        this.button1.style('background', 'lightgrey');
+    }
+
+    display(){
+        this.button1.mousePressed(() => {
+            if(system.authenticate(accessCode1, this.access1.value())){
+                this.button1.hide();
+                this.access1.hide();
+                score++;
+            }
+        });
+     }
+}
---
+++ System.js
@@ -0,0 +1,9 @@
+class System{
+    constructor(){}
+        authenticate(actualCode, enteredCode){
+            if(actualCode === enteredCode)
+               return true
+            else
+               return false
+        }
+}
